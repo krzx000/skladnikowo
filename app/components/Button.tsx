413b6d7e9c03c7dd6b63f9ce225c7133e7774537
krzx <padlo.krzysiek@gmail.com>
2025-10-19 17:57:58 +0200

    refactor: poprawa stylów w komponencie Button poprzez dodanie prefiksu 'enabled:' do klas hover i active

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -18,7 +18,7 @@ export const Button: React.FC<ButtonProps> = ({
         icon ? "flex items-center gap-2" : ""
       }  px-6 py-3.5 text-white ${
         disabled ? "cursor-not-allowed" : "cursor-pointer"
-      } rounded-full bg-orange font-bold text-sm whitespace-nowrap hover:scale-x-[1.025] hover:scale-y-[1.05] active:scale-y-[0.95] active:scale-x-[1.1] disabled:bg-orange/75 transition-all ${
+      } rounded-full bg-orange font-bold text-sm whitespace-nowrap enabled:hover:scale-x-[1.025] enabled:hover:scale-y-[1.05] enabled:active:scale-y-[0.95] enabled:active:scale-x-[1.1] disabled:bg-orange/75 transition-all ${
         className || ""
       }`}
       {...props}
